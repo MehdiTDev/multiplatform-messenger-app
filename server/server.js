dae6707fb239fc5247e51f432b8a205e0417be92
Mehdi <meh.tavakoli@gmail.com>
2025-04-18 16:08:10 +0200

    db.js created

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
 
-const app = express();
 dotenv.config();
+console.log("MONGO_URI:", process.env.MONGO_URI);
+connectDB();
+const app = express();
 
 app.get("/", (req, res) => res.send("API is running!"));
 
