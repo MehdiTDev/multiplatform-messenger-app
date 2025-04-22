623d76e2d59f76061feebae7b5660a2a52bfa5de
Mehdi <meh.tavakoli@gmail.com>
2025-04-22 13:15:39 +0200

    color package installed and used.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const colors = require("colors");
 
 dotenv.config();
 console.log("MONGO_URI:", process.env.MONGO_URI);
@@ -22,4 +23,6 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`.yellow.bold)
+);
