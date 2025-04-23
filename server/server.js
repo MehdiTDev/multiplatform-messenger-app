25f05157f6a09db11a7ca819c2a323f9fccf01a1
Mehdi <meh.tavakoli@gmail.com>
2025-04-23 13:04:37 +0200

    error handler added.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 
@@ -16,6 +17,9 @@ app.use(express.json()); // to accept JSON data
 app.get("/", (req, res) => res.send("API is running!"));
 
 app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
