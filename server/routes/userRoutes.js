4e3ebfa5644cb7cd7175e2bd6bd47d441b2bbbd6
Mehdi <meh.tavakoli@gmail.com>
2025-04-23 11:36:14 +0200

    password encrypted.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userControllers");
+const { registerUser, authUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
-router
-  .route("/")
-  .get(() => {})
-  .post(registerUser);
+router.route("/").post(registerUser);
 
 router.post("/login", authUser);
 
