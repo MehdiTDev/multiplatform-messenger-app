2a1f8b7d591d5078a99780e97703ff1db45cb3f1
Mehdi <meh.tavakoli@gmail.com>
2025-04-30 18:21:58 +0200

    Modal modified

--- client/components/miscellaneous/ProfileModal.js
+++ client/components/miscellaneous/ProfileModal.js
@@ -25,44 +25,44 @@ export default function ProfileModal({ user, children }) {
       )}
 
       {/* <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-        size="lg"
-        style={{ justifyContent: "center", alignItems: "center" }}
-      >
-        <Modal.Content style={{ width: "90%", maxHeight: 410, padding: 20 }}>
-          <Modal.Header
-            style={{ justifyContent: "center", alignItems: "center" }}
-          >
-            <Text style={{ fontSize: 40, fontFamily: "Work Sans" }}>
-              {user.name}
-            </Text>
-          </Modal.Header>
-          <Modal.Body
-            style={{
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Image
-              style={{ borderRadius: 75, width: 150, height: 150 }}
-              source={{ uri: user.pic }}
-              alt={user.name}
-            />
-            <Text
-              style={{ fontSize: 28, fontFamily: "Work Sans", marginTop: 20 }}
+          isOpen={isOpen}
+          onClose={onClose}
+          size="lg"
+          style={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <Modal.Content style={{ width: "90%", maxHeight: 410, padding: 20 }}>
+            <Modal.Header
+              style={{ justifyContent: "center", alignItems: "center" }}
             >
-              Email: {user.email}
-            </Text>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onPress={onClose}>
-              <Text>Close</Text>
-            </Button>
-          </Modal.Footer>
-        </Modal.Content>
-      </Modal> */}
+              <Text style={{ fontSize: 40, fontFamily: "Work Sans" }}>
+                {user.name}
+              </Text>
+            </Modal.Header>
+            <Modal.Body
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                style={{ borderRadius: 75, width: 150, height: 150 }}
+                source={{ uri: user.pic }}
+                alt={user.name}
+              />
+              <Text
+                style={{ fontSize: 28, fontFamily: "Work Sans", marginTop: 20 }}
+              >
+                Email: {user.email}
+              </Text>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onPress={onClose}>
+                <Text>Close</Text>
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal> */}
     </>
   );
 }
