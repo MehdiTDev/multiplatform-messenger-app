3f737d784c1798481c32f090beeef03569d3bdc1
Mehdi <meh.tavakoli@gmail.com>
2025-04-30 15:27:52 +0200

    fontAwesome packages installed

--- client/components/miscellaneous/SideDrawer.js
+++ client/components/miscellaneous/SideDrawer.js
@@ -1,10 +1,20 @@
 import React from "react";
-import { Box, Text } from "native-base";
+import { Box, Text, Tooltip } from "native-base";
+import { Pressable } from "react-native";
 
 const SideDrawer = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
+
   return (
     <Box>
-      <Text>Side Drawer</Text>
+      <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+        <Pressable varient="ghost">
+          <i class="fas fa-search"></i>
+        </Pressable>
+      </Tooltip>
     </Box>
   );
 };
