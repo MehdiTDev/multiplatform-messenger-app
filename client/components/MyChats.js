da0dad92ff83be352980c9cd092e37cbc025d70d
Mehdi <meh.tavakoli@gmail.com>
2025-05-02 23:52:23 +0200

    Chat list adjusted to stay inside the box

--- client/components/MyChats.js
+++ client/components/MyChats.js
@@ -1,14 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Box,
-  Text,
-  useToast,
-  Button,
-  Stack,
-  Icon,
-  PresenceTransition,
-  TouchableOpacity,
-} from "native-base";
+import { Box, Text, useToast, Stack } from "native-base";
 import { ChatState } from "../Context/ChatProvider";
 import ChatLoading from "./ChatLoading";
 import { getSender } from "../config/ChatLogics";
@@ -36,7 +27,7 @@ export default function MyChats() {
       setChats(data);
     } catch (error) {
       toast({
-        title: "Error Occured!",
+        title: "Error Occurred!",
         description: "Failed to Load the chats",
         status: "error",
         duration: 5000,
@@ -53,13 +44,14 @@ export default function MyChats() {
 
   return (
     <Box
-      flexDir={{ base: selectedChat ? "none" : "column", md: "column" }}
+      flexDir="column"
       alignItems="center"
       p={3}
       bg="white"
       w={{ base: "100%", md: "31%" }}
       borderRadius="lg"
       borderWidth="1px"
+      h="100%"
     >
       <Box
         pb={3}
@@ -77,7 +69,7 @@ export default function MyChats() {
             style={{
               paddingVertical: 8,
               paddingHorizontal: 16,
-              backgroundColor: "#ADD8E6", // Purple color equivalent
+              backgroundColor: "#ADD8E6",
               borderRadius: 8,
               alignItems: "center",
               flexDirection: "row",
@@ -90,41 +82,40 @@ export default function MyChats() {
       </Box>
 
       <Box
-        flexDir="column"
-        p={3}
+        flex={1}
         bg="#F8F8F8"
         w="100%"
-        h="100%"
         borderRadius="lg"
-        overflowY="hidden"
+        px={3}
+        py={3}
+        overflowY="auto"
       >
         {chats ? (
           <Stack space={3}>
             {chats.map((chat) => (
-              <Box
-                key={chat._id}
-                onPress={() => setSelectedChat(chat)}
-                cursor="pointer"
-                bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
-                color={selectedChat === chat ? "white" : "black"}
-                px={3}
-                py={2}
-                borderRadius="lg"
-              >
-                <Text>
-                  {!chat.isGroupChat
-                    ? getSender(loggedUser, chat.users)
-                    : chat.chatName}
-                </Text>
-                {chat.latestMessage && (
-                  <Text fontSize="xs">
-                    <b>{chat.latestMessage.sender.name} : </b>
-                    {chat.latestMessage.content.length > 50
-                      ? chat.latestMessage.content.substring(0, 51) + "..."
-                      : chat.latestMessage.content}
+              <Pressable key={chat._id} onPress={() => setSelectedChat(chat)}>
+                <Box
+                  bg={selectedChat === chat ? "#38B2AC" : "#E8E8E8"}
+                  color={selectedChat === chat ? "white" : "black"}
+                  px={3}
+                  py={2}
+                  borderRadius="lg"
+                >
+                  <Text>
+                    {!chat.isGroupChat
+                      ? getSender(loggedUser, chat.users)
+                      : chat.chatName}
                   </Text>
-                )}
-              </Box>
+                  {chat.latestMessage && (
+                    <Text fontSize="xs">
+                      <b>{chat.latestMessage.sender.name} : </b>
+                      {chat.latestMessage.content.length > 50
+                        ? chat.latestMessage.content.substring(0, 51) + "..."
+                        : chat.latestMessage.content}
+                    </Text>
+                  )}
+                </Box>
+              </Pressable>
             ))}
           </Stack>
         ) : (
